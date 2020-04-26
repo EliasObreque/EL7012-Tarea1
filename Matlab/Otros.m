831c67ac991ae664ca42623f62dfa85baa349084
MaibethSanchez <maibethsanchez91@gmail.com>
2020-04-26 14:25:02 -0400

    Script para generar Datos. Faltan detalles

--- Matlab/Otros.m
+++ Matlab/Otros.m
@@ -1,21 +1,27 @@
-%Generar señal PBRS de [0,1] de período máximo
-a_k=[1 0 0 1]'; %coeficientes para n=4
-s=ones(Pmax,1);
-for k=2:Pmax
-    if (k<=n)
-       s(k)=mod(sum(s(1:k-1).*a_k(1:k-1)),2);
-    else
-       s(k)=mod(sum(s(k-n:k-1).*a_k),2);
-    end
-end
-
-u=zeros(Ns*Pmax,1);
-for k=1:Pmax
-    u((k-1)*Ns+1:k*Ns)=s(k);
-end
-
-%Generar PBRS de [a,b]
-%u(k)=(b-a)*u(k)+a
-u=(b-a)*u+a;
-
+% %Generar señal PBRS de [0,1] de período máximo
+% a_k=[1 0 0 1]'; %coeficientes para n=4
+% s=ones(Pmax,1);
+% for k=2:Pmax
+%     if (k<=n)
+%        s(k)=mod(sum(s(1:k-1).*a_k(1:k-1)),2);
+%     else
+%        s(k)=mod(sum(s(k-n:k-1).*a_k),2);
+%     end
+% end
+% 
+% u=zeros(Ns*Pmax,1);
+% for k=1:Pmax
+%     u((k-1)*Ns+1:k*Ns)=s(k);
+% end
+% 
+% %Generar PBRS de [a,b]
+% %u(k)=(b-a)*u(k)+a
+% u=(b-a)*u+a;
 %-------------
+
+nRows = 100; % number of rows
+nSample = 10; % number of samples
+
+rndIDX = randperm(nRows);
+
+newSample = data(rndIDX(1:nSample), :);
