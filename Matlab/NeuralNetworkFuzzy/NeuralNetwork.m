ae2091ec8f99ae3883509963387cd0d2cd93bb8e
EliasObreque <els.obrq@gmail.com>
2020-05-06 01:48:28 -0400

    Update NeuralNetwork.m

--- Matlab/NeuralNetworkFuzzy/NeuralNetwork.m
+++ Matlab/NeuralNetworkFuzzy/NeuralNetwork.m
@@ -1,6 +1,8 @@
 function [net_trained, tr] = NeuralNetwork(num_neu, x_data, y_data, train_prc, test_prc, val_prc)
+% Crea una capa oculta en la red
 net_fit = fitnet(num_neu,'trainlm');
 
+% Funcion de activacion 
 net_fit.layers{1}.transferFcn = 'tansig'; % tansig = tanh
 net_fit.divideFcn = 'dividerand';
 net_fit.divideParam.trainRatio = train_prc; 
@@ -9,12 +11,13 @@ net_fit.divideParam.valRatio = val_prc;
 
 
 %TRAINING PARAMETERS
-net_fit.trainParam.show=20;  %# of ephocs in display
-net_fit.trainParam.lr=0.05;  %learning rate
-net_fit.trainParam.epochs=1000;  %max epochs
+net_fit.trainParam.show = 20;  %# of ephocs in display
+net_fit.trainParam.lr = 0.05;  %learning rate
+net_fit.trainParam.epochs = 1000;  %max epochs
 %net_fit.trainParam.goal=0.05^2;  %training goal
-net_fit.performFcn='mse';  %Name of a network performance function %type help nnperformance
 
+%Name of a network performance function %type help nnperformance
+net_fit.performFcn = 'mse';  
 
-[net_trained, tr] = train(net_fit, x_data, y_data);
+[net_trained, tr] = train(net_fit, x_data', y_data');
 return
